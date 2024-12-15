4d3cbd528841328c357c8c049dc5d4b5ab9452cb
Soumiyya Kamal <soumiyyakamal@Soumiyyas-Air.domain.name>
2024-12-15 22:14:07 +0530

    Intial commit

---
+++ cypress/e2e/logout.cy.js
@@ -0,0 +1,13 @@
+describe("Login", () => {
+
+})
+    it("login", () => {
+        cy.visit("https://tlm.epaysystems.com/WebPunch/US/V5.0/Login.aspx?ReturnUrl=%2fWebPunch%2fUS%2fV5.0%2fPunchInOut%2fPunchInOut.aspx")
+        cy.get('#txtCorpID').type('vensurehcm')
+        cy.get('#txtLoginID').type('suhail.thajudeen')
+        cy.get('#txtPassword').type('Apple@123')
+        cy.get('#btnLogin').click()
+
+        cy.get('#btnPunchOutNewMobile').click()
+
+    })
